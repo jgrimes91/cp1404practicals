680d3e4da2afce82e08380c134ccc10b3bb29de0
jessica.grimes@my.jcu.edu.au <Rotom2104>
2018-08-21 13:24:42 +1000

    List created from user input. -Print list functions.

---
+++ prac_04/numbers.py
@@ -0,0 +1,12 @@
+def main():
+    numbers = []
+    for i in range(5):
+        i = int(input("Please enter a number: "))
+        numbers.append(i)
+    print("The first number is", numbers[0])
+    print("The last number is", numbers[-1])
+    print("The smallest number is", min(numbers))
+    print("The largest number is", max(numbers))
+    print("The average of the numbers is", sum(numbers)/5)
+
+main()
