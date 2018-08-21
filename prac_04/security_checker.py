1166d5181ff839ca42205679ccbb1aac204980b5
jessica.grimes@my.jcu.edu.au <Rotom2104>
2018-08-21 13:33:26 +1000

    While loop checking username in list

---
+++ prac_04/security_checker.py
@@ -0,0 +1,14 @@
+def main():
+    usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+                 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer',
+                 'bob']
+    user_name = input("Please enter your username: ")
+
+    while user_name not in usernames:
+        print("Access Denied")
+        user_name = input(">>> ")
+    else:
+        print("Access Granted")
+
+
+main()
